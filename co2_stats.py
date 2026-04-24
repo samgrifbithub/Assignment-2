b18dcc0f238d1a6fcc9dcee06a724e842a280549
samgrifbithub <sgriff31@calpoly.edu>
2026-04-24 13:40:13 -0700

    Create co2_stats.py

---
+++ co2_stats.py
@@ -0,0 +1,11 @@
+import csv
+from typing import *
+from dataclasses import dataclass
+import unittest
+import math
+import sys
+sys.setrecursionlimit(10**6)
+class Tests(unittest.TestCase):
+pass
+if (__name__ == '__main__'):
+unittest.main()
