ceb5bc03d12d970e7c346d8d4ac056ba409770b4
kdosanjh <kdosanjh@calpoly.edu>
2026-04-24 20:52:38 -0700

    i made the class rlnode and row

--- co2_stats.py
+++ co2_stats.py
@@ -5,6 +5,20 @@ import unittest
 import math
 import sys
 sys.setrecursionlimit(10**6)
+
+@dataclass(frozen=True)
+class Row:
+    country: str
+    year: int
+    emission_type: str
+    emissions: float
+
+@dataclass(frozen=True)
+class RLNode:
+    first: Row
+    rest: Optional['RLNode']
+
+
 class Tests(unittest.TestCase):
 pass
 if (__name__ == '__main__'):
